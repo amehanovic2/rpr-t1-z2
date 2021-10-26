d01ed3502667de63daab1c13abcbe81dc232dd26
Adna <amehanovic2@etf.unsa.ba>
2021-10-26 21:22:12 +0200

    Initial commit

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static int sumaCifara(int broj) {
-        int suma=0;
-        while(broj!=0){
+        int suma = 0;
+        while(broj != 0){
             suma = suma + broj % 10;
             broj = broj / 10;
         }
@@ -15,7 +15,7 @@ public class Main {
         Scanner ulaz = new Scanner(System.in);
         int n= ulaz.nextInt();
         System.out.println("Brojevi koji su djeljivi sa sumom svojih cifara:");
-        for(int i=1;i<=n;i++) {
+        for(int i = 1;i <= n;i ++) {
             if (i % sumaCifara(i) == 0)
                 System.out.println(i);
         }
